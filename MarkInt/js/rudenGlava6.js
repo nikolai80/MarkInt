bc5c82c5fdcff192efd6fcc2d6c36f87831d9551
unknown <D:\TheBatBases>
2015-06-01 16:35:07 +0300

    выполнение заданий по 6 главе ruden

---
+++ MarkInt/js/rudenGlava6.js
@@ -0,0 +1,10 @@
+﻿(function($) {
+		$(document).ready(function() {
+			$("#inputEmail5").val("Селектор по id");
+			$("div").css("border", "9px solid red");
+			$(".form-group").css("border", "3px solid green");
+
+			}
+		);
+})(jQuery);
+
