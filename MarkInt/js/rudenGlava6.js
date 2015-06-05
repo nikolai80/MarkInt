ca314ae81f7100edb35ac5bde93be29fe2d03122
unknown <D:\TheBatBases>
2015-06-05 14:50:02 +0300

    no message

--- MarkInt/js/rudenGlava6.js
+++ MarkInt/js/rudenGlava6.js
@@ -3,6 +3,11 @@
 			$("#inputEmail5").val("Селектор по id");
 			$("div").css("border", "9px solid red");
 			$(".form-group").css("border", "3px solid green");
+			$(".headerPhone.btn").css("border", "3px dashed rgb(128, 0, 115)");
+			var count = $(".wrapper>*").length;
+			alert(count);
+			$("label+div").text("Мне нравится этот селектор");
+			$("h4~button").text("вообще не приходилось никогда использовать этот селектор");
 
 			}
 		);
