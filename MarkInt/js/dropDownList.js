37ae395953b2862bce40596210d10388ccd91ba9
unknown <D:\TheBatBases>
2015-06-09 16:59:14 +0300

    dropDowmList

---
+++ MarkInt/js/dropDownList.js
@@ -0,0 +1,7 @@
+﻿$(document).ready(function () {
+	$("ul").hide();
+	$("ul li:odd").css("background-color", "#efefef");
+	$("h3 span").click(function () {
+		$(this).parent().next().slideToggle();
+	});
+});
